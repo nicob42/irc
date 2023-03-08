925d0e5187d017c0ae20fd2519a38b60fc565ad7
Alrusso42 <100759338+Alrusso42@users.noreply.github.com>
2023-03-08 17:58:03 +0100

    Update ope.hpp

--- ft_irc/includes/commands/ope.hpp
+++ ft_irc/includes/commands/ope.hpp
@@ -1,52 +1,51 @@
 #ifndef OPE_H
 #define OPE_H
 
+// Our includes
 #include "../Command.hpp"
 
-class Ope : public Command  // Classe ope hérite de la classe Command
+class Ope : public Command
 {
   public:
-    // Constructeur de la classe ope
-    Ope()
-    {
-        name = "ope"; // Nom de la commande
-        description = "Give operator role to yourself or a client"; // Description de la commande
-        usage = "ope <nikname?>"; // Usage de la commande
-        example[0] = "ope"; // Exemple d'utilisation de la commande
-        example[1] = "ope Faerris"; // Exemple d'utilisation de la commande avec un argument
-        ope_required = false; // Indique si l'utilisateur qui exécute la commande est déjà un opérateur ou non
-    }
+	Ope()
+	{
+		_name = "ope";
+		_description = "Assign operator role yourself or a client";
+		_usage = "ope <nikname?>";
+		_example[0] = "ope";
+		_example[1] = "ope alfred";
+		_is_ope = false;
+	}
 
-    void execute()
-    {
-        if (message->getParams().size() == 0) // Si aucun argument n'est fourni
-        {
-            sender->ope_required = !sender->ope_required; // Inverse le statut d'opérateur de l'utilisateur qui exécute la commande
-            sender->message(sender->ope_required ? "Congrats, you're now operator!\n" : "Unfortunately you have been demoted soldier\n"); // Envoie un message à l'utilisateur pour confirmer ou annuler son statut d'opérateur
-        }
-        else if (message->getParams().size() > 1) // Si trop d'arguments sont fournis
-        {
-            sender->message("Wrong usage, use it this way : ope <nikname?>\n"); // Envoie un message d'erreur à l'utilisateur
-        }
-        else // Si un argument est fourni
-        {
-            Client *client;
-            if ((client = server->getClient(message->getParams()[0]))) // Recherche le client correspondant au pseudonyme fourni
-            {
-                client->ope_required = !client->ope_required; // Inverse le statut d'opérateur du client
-                sender->message(std::string(client->ope_required ?
-                                                 "Currently the client " + client->nick + " is an operator!\n" :
-                                                 "Currently the client " + client->nick + " isn't an operator!\n")
-                                     .c_str()); // Envoie un message à l'utilisateur pour confirmer le statut d'opérateur du client
-                client->message(
-                    std::string(client->ope_required ? "Congrats you're an operator!\n" : "Unfortunately you aren't an operator!\n")
-                        .c_str()); // Envoie un message au client pour confirmer son nouveau statut d'opérateur
-                return;
-            }
-            else
-                sender->message("Client not found!\n"); // Si aucun client ne correspond au pseudonyme fourni, envoie un message d'erreur à l'utilisateur
-        }
-    }
+	void execute()
+	{
+		if (_message->getParams().size() == 0)
+		{
+			_sender->_is_ope = !_sender->_is_ope;
+			_sender->message(_sender->_is_ope ? "Now you're operator!\n" : "Ope role removed!\n");
+		}
+		else if (_message->getParams().size() > 1)
+		{
+			_sender->message("Please send valid params! Ex: ope <nikname?>\n");
+		}
+		else
+		{
+			Client *client;
+			if ((client = _server->getClient(_message->getParams()[0])))
+			{
+				client->_is_ope = !client->_is_ope;
+				_sender->message(std::string(client->_is_ope ?
+				                                 "Now the client " + client->_nick + " is an operator!\n" :
+				                                 "Now the client " + client->_nick + " isn't an operator!\n")
+				                     .c_str());
+				client->message(
+				    std::string(client->_is_ope ? "Now you're an operator!\n" : "Now you aren't an operator!\n")
+				        .c_str());
+				return;
+			}
+			else
+				_sender->message("Client not found!\n");
+		}
+	}
 };
-
 #endif
