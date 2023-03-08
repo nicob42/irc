d19d969432337a84ba9e5fd2061b7bb43ffaf2f5
Alrusso42 <100759338+Alrusso42@users.noreply.github.com>
2023-03-08 17:57:05 +0100

    Update help.hpp

--- ft_irc/includes/commands/help.hpp
+++ ft_irc/includes/commands/help.hpp
@@ -1,53 +1,48 @@
 #ifndef HELP_H
 #define HELP_H
 
-#include "../Color.hpp"  // Inclusion de la classe Color pour afficher des couleurs dans le terminal
-#include "../Command.hpp"  // Inclusion de la classe Command dont la classe Help hérite
+// Our includes
+#include "../Command.hpp"
 
-class Help : public Command // Classe Help hérite de la classe Command
+class Help : public Command
 {
   public:
-    // Constructeur de la classe Help
-    Help()
-    {
-        name = "help";  // Nom de la commande
-        description = "Show you a guide about usage of all command";  // Description de la commande
-        usage = "help <command?>";  // Utilisation de la commande
-        example[0] = "help";  // Exemple d'utilisation de la commande
-        example[1] = "help ping";  // Exemple d'utilisation de la commande avec un argument
-    }
+	Help()
+	{
+		_name = "help";
+		_description = "See a guide about each implemented command";
+		_usage = "help <command?>";
+		_example[0] = "help";
+		_example[1] = "help ping";
+	}
 
-    // Méthode qui est exécutée lorsqu'on utilise la commande Help
-    void execute() 
-    {
-        std::string result = "";  // Variable qui va stocker le résultat de l'exécution de la commande
-        bool filter = false;  // Booléen qui va déterminer si on filtre les résultats de la commande en fonction d'un argument
-        std::map<std::string, Command *>::iterator it = server->commands.begin();  // Itérateur qui va parcourir toutes les commandes du serveur
-        std::map<size_t, std::string> params = message->getParams();  // Récupération des arguments passés avec la commande Help
+	void execute()
+	{
+		std::string                                res = "";
+		bool                                       filtering = false;
+		std::map<std::string, Command *>::iterator it = _server->_commands.begin();
+		std::map<size_t, std::string> params = _message->getParams();
 
-        result = std::string(C_U) + std::string(C_BOLD) + std::string(C_GREEN) + "Commands usage guide" + std::string(C_X) + "\n";  // Ajout d'un titre en vert gras souligné au résultat
+		res = std::string(C_U) + std::string(C_BOLD) + std::string(C_GREEN) + "Commands usage guide" + std::string(C_X) + "\n";
 
-        if (params.size() == 1 && server->commands.count(params[0]))  // Si un argument a été passé avec la commande, on filtre les résultats
-            filter = true;
+		if (params.size() == 1 && _server->_commands.count(params[0]))
+			filtering = true;
 
-        for (; it != server->commands.end(); it++)  // On parcourt toutes les commandes du serveur
-        {
-            if (filter && it->second->getName() != params[0])  // Si on filtre les résultats et que la commande actuelle ne correspond pas à l'argument passé, on continue
-                continue;
+		for (; it != _server->_commands.end(); it++)
+		{
+			if (filtering && it->second->getName() != params[0])
+				continue;
+			res += std::string("Command: " + it->second->getName() +
+			                   "\n\t Description: " + it->second->getDescription() +
+			                   "\n\t Usage: " + it->second->getUsage() + "\n\t Example:\n");
 
-            result += std::string("Command: " + it->second->getName() +  // Ajout du nom de la commande
-                                   "\n\t Description: " + it->second->getDescription() +  // Ajout de la description de la commande
-                                   "\n\t Usage: " + it->second->getUsage() + "\n\t Example:\n");  // Ajout de l'utilisation de la commande et d'une ligne pour les exemples
-
-            std::map<size_t, std::string> example(it->second->getExample());  // Récupération des exemples de la commande actuelle
-            std::map<size_t, std::string>::iterator it_ex = example.begin();  // Itérateur qui va parcourir tous les exemples
-            for (; it_ex != example.end(); it_ex++)  // On parcourt tous les exemples
-                result += std::string("\t\t" + it_ex->second + "\n");  // Ajout de l'exemple
-
-            result += "\n";  // Ajout d'une ligne vide à la fin de chaque commande
-        }
-
-        sender->message(result.c_str());  // Envoi du résultat de la commande au client
-    }
+			std::map<size_t, std::string> example(it->second->getExample());
+			std::map<size_t, std::string>::iterator it_ex = example.begin();
+			for (; it_ex != example.end(); it_ex++)
+				res += std::string("\t\t" + it_ex->second + "\n");
+			res += "\n";
+		}
+		_sender->message(res.c_str());
+	}
 };
-#endif 
+#endif
