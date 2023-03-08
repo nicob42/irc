12161211f613ce1194d874f97b4119a05b9623f7
Alrusso42 <100759338+Alrusso42@users.noreply.github.com>
2023-03-08 17:58:34 +0100

    Update ping.hpp

--- ft_irc/includes/commands/ping.hpp
+++ ft_irc/includes/commands/ping.hpp
@@ -1,26 +1,23 @@
 #ifndef PING_H
 #define PING_H
 
+// Our includes
 #include "../Command.hpp"
 
-class Ping : public Command // Définition de la classe Ping, qui hérite de Command
+class Ping : public Command
 {
   public:
-    // Constructeur de la classe
-    Ping()
-    {
-        // Initialise les informations de la commande
-        name = "ping"; // Nom de la commande
-        description = "Determine server status"; // Description de la commande
-        usage = "ping"; // Utilisation de la commande
-        example[0] = "ping"; // Exemple d'utilisation de la commande
-    }
+	Ping()
+	{
+		_name = "ping";
+		_description = "Just ping-pong the server";
+		_usage = "ping";
+		_example[0] = "ping";
+	}
 
-    // Méthode appelée lors de l'exécution de la commande
-    void execute()
-    {
-        sender->message("pong\n"); // Envoie un message "pong" au client exécutant la commande
-    }
+	void execute()
+	{
+		_sender->message("pong\n");
+	}
 };
-
 #endif
